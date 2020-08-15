132dec193bc5d8af9e1faae18b0c4e3c21045af3
Sergey Denisenko <sd.a2@yandex.ru>
2020-08-15 21:12:36 +0300

    Updates

---
+++ precache-manifest.642e539bc268445c5437e3c0eb2dfa8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878dfcd8e81d8132a32030d540348121",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "a98aa5d66097f73033e2",
+    "url": "/mesto-react/static/css/main.c371ccdb.chunk.css"
+  },
+  {
+    "revision": "6559dbef5c3552dfe58b",
+    "url": "/mesto-react/static/js/2.f5da3a84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.f5da3a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98aa5d66097f73033e2",
+    "url": "/mesto-react/static/js/main.6cb1b822.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/mesto-react/static/media/Trash.d8078343.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/mesto-react/static/media/Vector_white.c6f11019.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/mesto-react/static/media/heart_like.83a46f45.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/mesto-react/static/media/heart_like_black.5bbb05f7.svg"
+  }
+]);
